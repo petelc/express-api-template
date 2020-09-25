4eb69960a4016a20436b740698701da67e9e6d8c
Pete Carroll <petelc1966@hotmail.com>
2020-09-25 04:24:19 -0400

    setup tests and run

---
+++ test/setup.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable linebreak-style */
+import supertest from 'supertest';
+import chai from 'chai';
+import sinonChai from 'sinon-chai';
+import app from '../src/app';
+
+chai.use(sinonChai);
+export const { expect } = chai;
+export const server = supertest.agent(app);
+export const BASE_URL = '/v1';

8cebe80fe3f7bd63945568aa17ab775dc6944113
Pete Carroll <petelc1966@hotmail.com>
2020-09-26 06:16:16 -0400

    successfully connected to local postgresql

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable linebreak-style */
 import express from 'express';
 
-import { indexPage, aboutPage } from '../controllers';
+import { indexPage, aboutPage, messagePage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 
 indexRouter.get('/about', aboutPage);
+
+indexRouter.get('/messages', messagePage);
 
 export default indexRouter;
--- src/settings.js
+++ src/settings.js
@@ -3,3 +3,4 @@ import dontenv from 'dotenv';
 
 dontenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
+export const connectionString = process.env.CONNECTION_STRING;
